e5ceb25f751f0ea8b5c745f252461cc6b5e9170e
Limitles-369 <akashsamui369@gmail.com>
2024-11-21 16:24:22 +0530

    Hedder_Footer

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<!-- header -->
+<?php require("./views/mybase/header.php"); ?>
+<!-- header end -->
+
 <!-- navbar -->
       <?php require("./views/mybase/nav_bar.php"); ?>
+      <h1 style="Display: flex; justify-content: center; align-items: center; 
+      height: 100vh; font-size: 50px; color: black; background-color: white; 
+      text-align: center; text-transform: uppercase; font-family: 'Times New Roman',
+       Times, serif;">Welcome To My Home Page</h1>
 <!-- //navbar end -->
- <h1 style="Display: flex; justify-content: center; align-items: center; height: 100vh; font-size: 50px; color: black; background-color: white; text-align: center; text-transform: uppercase; font-family: 'Times New Roman', Times, serif;">Welcome To My Home Page</h1>
-      <?php require("./views/mybase/nav_bar.php"); ?>
+
+<!-- footer -->
+<?php require("./views/mybase/footer.php"); ?>
+<!-- footer end -->
+
+      
--- views/mybase/nav_bar.php
+++ views/mybase/nav_bar.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<link rel="stylesheet" href="./asset/css/style.css">
-<title>PROJECT_1</title>
-</head>
-<body>
 <nav class="navbar navbar-expand-lg navbar-light bg-primary">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
